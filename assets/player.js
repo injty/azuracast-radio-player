eb6bdf095ce7f03b1a5bcd73c2465cd8d2811bfd
nijat ibrahimov <nijatibrahimov@gmail.com>
2022-10-04 18:42:43 +0400

    add: title, art.

--- assets/player.js
+++ assets/player.js
@@ -1,64 +1,92 @@
-// requests
-// --------
-const request = (url) => fetch(url).then((res) => res.json());
-const station = "https://media.money4you.financial:8032/api/station/1";
-const nowplaying = "https://media.money4you.financial:8032/api/nowplaying";
+document.addEventListener("DOMContentLoaded", player);
 
-// varables
-// --------
-const playerImage = document.querySelector("#playerImage");
-const playerTitle = document.querySelector("#playerTitle");
-const playerArtist = document.querySelector("#playerArtist");
-const playerAudio = document.querySelector("#playerAudio");
-const playerBtnPlay = document.querySelector("#playerBtnPlay");
+function player() {
+	// requests
+	// --------
+	const request = (url) => fetch(url).then((res) => res.json());
+	const station = "https://media.money4you.financial:8032/api/station/1";
+	const nowplaying = "https://media.money4you.financial:8032/api/nowplaying";
+	const src = "https://media.money4you.financial:8033/radio.mp3";
 
-// templates
-// ---------
-const playerAudioTemp = `
+	// varables
+	// --------
+	const playerImage = document.querySelector("#playerImage");
+	const playerTitle = document.querySelector("#playerTitle");
+	const playerArtist = document.querySelector("#playerArtist");
+	const playerAudio = document.querySelector("#playerAudio");
+	const playerBtnPlay = document.querySelector("#playerBtnPlay");
+
+	// templates
+	// ---------
+	const playerAudioTemp = `
 	<source src="%src%"></source>
 `;
-const playerImageTemp = `
+	const playerImageTemp = `
 	<img class="pl-image__source" src="%src%" alt="Album image">
 `;
-const playerTextTemp = `
+	const playerTextTemp = `
 	<h3 class="pl-text">%text%</h3>
 `;
 
-// main
-// ----
-const Player = {
-	getStation: request(station),
-	getNowplaying: request(nowplaying),
+	// debounce
+	// --------
+	const debounce = (fn, ms) => {
+		let timer;
 
-	play: () => {
-		playerBtnPlay.addEventListener("click", () => {
-			playerAudio.paused ? playerAudio.play() : playerAudio.pause();
-		});
-	},
+		return function () {
+			clearInterval(timer);
+			timer = setInterval(() => {
+				fn.apply(this, arguments);
+			}, ms);
+		};
+	};
 
-	init: async function fn() {
-		this.data = await this.getStation.then((res) => res);
-		this.title = await this.getNowplaying.then(
-			(res) => res[0].now_playing.song.text
-		);
-		this.art = await this.getNowplaying.then(
-			(res) => res[0].now_playing.song.art
-		);
-		this.track = await this.getStation.then((res) => res.listen_url);
-		this.station = await this.getStation.then((res) => res.name);
+	// main
+	// ----
+	const Player = {
+		main: this.main,
+		src: this.src,
+		remaining: this.remaining,
+		art: this.art,
+		title: this.title,
 
-		console.log(this.data);
+		_update: async function () {
+			await request(nowplaying).then((res) => {
+				this.src = src;
+				this.main = res[0].now_playing;
+				this.remaining = Number(res[0].now_playing.remaining * 1000);
+				this.art = res[0].now_playing.song.art;
+				this.title = res[0].now_playing.song.text;
+			});
+			await this._render();
+		},
 
-		const start = () => {
+		_play: async function () {
+			await this._debounce();
+			playerBtnPlay.addEventListener("click", () => {
+				playerAudio.paused ? playerAudio.play() : playerAudio.pause();
+			});
+		},
+
+		_render: function () {
 			playerImage.innerHTML = playerImageTemp.replace("%src%", this.art);
 			playerTitle.innerHTML = playerTextTemp.replace("%text%", this.title);
-			playerArtist.innerHTML = playerTextTemp.replace("%text%", "Artist");
-			playerAudio.innerHTML = playerAudioTemp.replace("%src%", this.track);
+			playerAudio.innerHTML = playerAudioTemp.replace("%src%", this.src);
+		},
 
-			this.play();
-		};
-		start();
-	},
-};
+		_debounce: function () {
+			debounce(async () => {
+				await this._update();
+				console.log(this.remaining);
+				// }, this.remaining)();
+			}, 1000)();
+		},
 
-Player.init();
+		init: async function () {
+			await this._update();
+			this._play();
+		},
+	};
+
+	Player.init();
+}
